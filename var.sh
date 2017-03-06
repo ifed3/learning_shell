b5d6c5428a9509692455b6c1a0986bf218e3c97e
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-03-06 11:23:37 -0800

    Intro to variables

---
+++ var.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Variables are defined with intermediate spacing between the variable name, value and equal sign
+# After being defined, the variable is called with a "$" before the name
+
+DOPE_BAND="Sylvan Esso"
+echo $DOPE_BAND
