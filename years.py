43c7692827ca82e05bdfbaf0fa52bc0ea071d415
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-03-07 14:56:43 -0800

    Add in conditionals

--- years.py
+++ years.py
@@ -6,8 +6,21 @@ import sys
 # Variable definition is done with the equal sign
 # argv is the list of arguments entered at runtime
 # argv[0] is the path of the script being run
-name = sys.argv[1]
-age = int(sys.argv[2]) # Type conversion is done as str() or float()
-diff = 100 - age
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    name = raw_input('What is your name? ')
 
-print "Hello {0}, you will be 100 in {1} years".format(name, diff)
+if len(sys.argv) > 2:
+    age = int(sys.argv[2]) # Type conversion is done as str() or float()
+else:
+    age = int(raw_input('How young are you? '))
+
+if age == 100:
+    comment = "You're already on the dot"
+elif age < 100:
+    comment = "You will be 100 in {} years".format(100 - age)
+else:
+    comment = "You were already 100 {} years ago".format(age - 100)
+
+print "Hello, {0}. {1}!".format(name, comment)
