849c20f35ebaf66979e252077ae92ee7eccfa9b9
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-03-07 13:59:59 -0800

    Wildcard within loop

--- loops.sh
+++ loops.sh
@@ -5,3 +5,10 @@ for i in 1 2 3 4 5
 do
     echo "Looping through values $i"
 done
+
+# What should this segment do? Expect that shell would act on each character
+# So the iteration would be hello 1 then all current files then 2 goodbye
+for i in hello 1 * 2 goodbye
+do
+    echo "Looping through values $i"
+done
