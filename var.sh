3d3f9b4fb289c898feb93d6eeaf078034bab941d
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-03-06 17:19:45 -0800

    Read stdin lesson

--- var.sh
+++ var.sh
@@ -5,3 +5,8 @@
 
 DOPE_BAND="Sylvan Esso"
 echo $DOPE_BAND
+
+# Read retrieves line from stdin
+echo What is your occupation?
+read MY_JOB
+echo "Good luck, robots will soon be a better $MY_JOB than you are!"
