0f98ca47dc1f5931f594acb69606dd314420b643
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2017-03-07 14:13:42 -0800

    Add while loops to be mix

--- loops.sh
+++ loops.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-# For Loops
-for i in 1 2 3 4 5
-do
-    echo "Looping through values $i"
-done
+# # For Loops
+    # for i in 1 2 3 4 5
+    # do
+    #     echo "Looping through values $i"
+    # done
 
-# What should this segment do? Expect that shell would act on each character
-# So the iteration would be hello 1 then all current files then 2 goodbye
-for i in hello 1 * 2 goodbye
-do
-    echo "Looping through values $i"
-done
+    # # What should this segment do? Expect that shell would act on each character
+    # # So the iteration would be hello 1 then all current files then 2 goodbye
+    # for i in hello 1 * 2 goodbye
+    # do
+    #     echo "Looping through values $i"
+    # done
+
+# While Loops
+    # INPUT="bounce it"
+    # while [ "$INPUT" != "exit" ]
+    # do
+    #     echo "Send me a message. To quit, type exit"
+    #     read INPUT
+    #     echo "That was: $INPUT"
+    # done
+
+    # Colon : represents true in while Loops
+    while :
+    do 
+        echo "Send me a message, To quit press Ctrl-C"
+        read INPUT
+        echo "That was: $INPUT"
+    done
+
